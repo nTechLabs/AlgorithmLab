47cec578d34286a716a8ce3a60c23b33b756682f
nTechLabs <zhl22pa@gmail.com>
2019-11-18 19:45:19 +0900

    Update 1_selectionSort.c

--- 1_selectionSort.c
+++ 1_selectionSort.c
@@ -2,10 +2,11 @@
 int main(void){
 	int i, j, index, min, temp;
 	int array[10] = {1, 10, 5, 8 ,7, 6 , 4, 3, 2, 9 };
-	
-	for(i=0; i<10; i++){
+	int len = sizeof(array)/sizeof(int);
+
+	for(i=0; i<len; i++){
 		min=9999;
-		for(j =i; j<10; j++){
+		for(j =i; j<len; j++){
 			if(min>array[j]){
 				min = array[j];
 				index=j;
@@ -16,7 +17,7 @@ int main(void){
 		array[index]=temp;
 	}
 	
-	for( i=0; i<10; i++){
+	for( i=0; i<len; i++){
 		printf("%d,", array[i]);
 	}
 	return 0;
